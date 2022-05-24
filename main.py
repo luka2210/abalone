1f7c9f6bc13dc8bd27002e4f2304c344f35b6920
luka2210 <lukamomcilovic221099@gmail.com>
2022-05-24 23:42:03 +0200

    odradjeni i softmax i linear output layer

--- main.py
+++ main.py
@@ -24,36 +24,62 @@ def load_and_preprocess_data():
     return X, Y
 
 
-def create_model(input_dim, hidden_layers):
+def create_model(hidden_layers, output_layer='linear'):
     model = tf.keras.Sequential()
 
     for layer_size in hidden_layers:
         model.add(tf.keras.layers.Dense(
             layer_size,
-            input_dim=input_dim,
             activation='leaky_relu',
             kernel_initializer='glorot_uniform',
-            bias_initializer="glorot_uniform",
-            #kernel_regularizer="l1"
+            bias_initializer='glorot_uniform'
         ))
-    model.add(tf.keras.layers.Softmax(-1))
+
+    if output_layer == 'softmax':
+        loss = 'sparse_categorical_crossentropy'
+        metrics = ['accuracy']
+        model.add(tf.keras.layers.Dense(
+            30,
+            activation='leaky_relu',
+            kernel_initializer='glorot_uniform',
+            bias_initializer='glorot_uniform'
+        ))
+        model.add(tf.keras.layers.Softmax(-1))
+    else:
+        loss = "mean_squared_error"
+        metrics = ['mean_absolute_error']
+        model.add(tf.keras.layers.Dense(
+            1,
+            activation='linear',
+            kernel_initializer='glorot_uniform',
+            bias_initializer='glorot_uniform'
+        ))
 
     model.compile(
         optimizer='adam',
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
+        loss=loss,
+        metrics=metrics
     )
 
     return model
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def main():
     X, Y = load_and_preprocess_data()
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
-    model = create_model(X.shape[1], [16, 8, 6, 4, 2, 30])
-    model.summary()
-    model.fit(X_train, Y_train, epochs=600, batch_size=512)
-    model.evaluate(X_test, Y_test)
+
+    softmax_model = create_model([16, 8, 6, 4, 2], 'softmax')
+    softmax_model.fit(X_train, Y_train, epochs=600, batch_size=256)
+    softmax_model.evaluate(X_test, Y_test)
+
+    linear_model = create_model([32, 16, 16, 8, 8, 4], 'linear')
+    linear_model.fit(X_train, Y_train, epochs=600, batch_size=256)
+    linear_model.evaluate(X_test, Y_test)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
